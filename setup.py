4d7985f73b7b6a35b6c513c35af38aff9fb7f7d8
joshz <jzegan@gmail.com>
2013-09-10 18:53:59 +0200

    added

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+
+setup(name='pipupgrade',
+      version='0.1.0',
+      author='Jan Zegan',
+      description='Check and/or update all Python packages with pip',
+      author_email='jzegan@gmail.com',
+      #packages=find_packages(),
+      install_requires=['pip'],
+      py_modules = ['scripts'],
+      entry_points={
+          'console_scripts': ['pipupgrade = scripts.pipupgrade:main']
+          }
+      )
